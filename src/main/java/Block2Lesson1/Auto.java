4094702100a4538b94b932936d179ae73ef9fa4d
Дмитрий <voyger0781@gmail.com>
2021-06-17 22:47:34 +0300

    0.2

--- src/main/java/Block2Lesson1/Auto.java
+++ src/main/java/Block2Lesson1/Auto.java
@@ -18,4 +18,17 @@ public class Auto implements Cloneable{
             return (Engine) super.clone();
         }
     }
+    public void setEngines(Engine...engines){
+        this.engines = engines;
+    }
+
+    public Auto clone() throws CloneNotSupportedException{          //метод для клонирования с объекта Auto с объектом класса Engine
+        Auto newAuto = (Auto) super.clone();                         // клонируем новый объект класса Auto
+        Engine[] newEngine = new Engine[newAuto.engines.length];     //создаем мвссив с объектами Engine.Длинна равна кол-ву объ_в Auto с объ_м engine
+        for (int i = 0;i<newAuto.engines.length;i++){                //идем циклом по массиву с объектами Auto и клонируем в их объект класса Engine
+            newEngine[i] = newAuto.engines[i].clone();               // в объект newAuto мы запихиваем склонированный объект engine
+        }
+        newAuto.setEngines(newEngine);                                //запихиваем в новый объект auto склонированный объект класса engine
+        return newAuto;                                               //возвращаем склонированный новый объект класса Auto
+    }
 }
