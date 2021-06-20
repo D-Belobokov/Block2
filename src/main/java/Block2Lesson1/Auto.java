a10956e7d4d163240cf368d10bc23010d215888c
Дмитрий <voyger0781@gmail.com>
2021-06-20 10:28:27 +0300

    1.1

--- src/main/java/Block2Lesson1/Auto.java
+++ src/main/java/Block2Lesson1/Auto.java
@@ -1,5 +1,7 @@
 package Block2Lesson1;
 
+import java.util.Arrays;
+
 public class Auto implements Cloneable{
     private final String name;
     private final String brand;
@@ -14,21 +16,35 @@ public class Auto implements Cloneable{
     }
     static class Engine implements Cloneable {
 
+        public Engine() {
+        }
+
         public Engine clone() throws CloneNotSupportedException {
             return (Engine) super.clone();
+
         }
     }
     public void setEngines(Engine...engines){
         this.engines = engines;
     }
 
-    public Auto clone() throws CloneNotSupportedException{          //метод для клонирования с объекта Auto с объектом класса Engine
-        Auto newAuto = (Auto) super.clone();                         // клонируем новый объект класса Auto
-        Engine[] newEngine = new Engine[newAuto.engines.length];     //создаем мвссив с объектами Engine.Длинна равна кол-ву объ_в Auto с объ_м engine
-        for (int i = 0;i<newAuto.engines.length;i++){                //идем циклом по массиву с объектами Auto и клонируем в их объект класса Engine
-            newEngine[i] = newAuto.engines[i].clone();               // в объект newAuto мы запихиваем склонированный объект engine
+    public Auto clone() throws CloneNotSupportedException{
+        Auto newAuto = (Auto) super.clone();
+        Engine[] newEngine = new Engine[newAuto.engines.length];
+        for (int i = 0;i<newAuto.engines.length;i++){
+            newEngine[i] = newAuto.engines[i].clone();
         }
-        newAuto.setEngines(newEngine);                                //запихиваем в новый объект auto склонированный объект класса engine
-        return newAuto;                                               //возвращаем склонированный новый объект класса Auto
+        newAuto.setEngines(newEngine);
+        return newAuto;
+    }
+
+    @Override
+    public String toString() {
+        return "Auto{" +
+                "name='" + name + '\'' +
+                ", brand='" + brand + '\'' +
+                ", model='" + model + '\'' +
+                ", engines=" + Arrays.toString(engines) +
+                '}';
     }
 }
--- src/main/java/Block2Lesson1/MainAuto.java
+++ src/main/java/Block2Lesson1/MainAuto.java
@@ -1,7 +1,18 @@
 package Block2Lesson1;
 
 public class MainAuto {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws CloneNotSupportedException {
+        Auto auto = new Auto("Dmitry","Ford","Focus");
+        Auto.Engine engine = new Auto.Engine();
+        Auto.Engine newEngine = new Auto.Engine();
 
+        auto.setEngines(newEngine);
+
+        Auto nextAuto = auto.clone();
+
+        System.out.println(engine);
+        System.out.println(newEngine);
+        System.out.println(auto);
+        System.out.println(nextAuto);
     }
 }
